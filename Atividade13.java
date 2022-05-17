e5f0c89f1d7f010d5aa5890f83a3c278e776af5b
P4TR1CK-R3N4N <105107792+P4TR1CK-R3N4N@users.noreply.github.com>
2022-05-16 21:40:23 -0300

    Atividades de Programação Java
    Atividades praticas em java.

---
+++ Atividade13.java
@@ -0,0 +1,21 @@
+//Nome do Autor: Patrick Renan Marques Barboza
+//Data de criação: 16/05/2022
+//salvar como Atividade13.java
+
+//nome da classe
+class Atividade13
+{
+	//módulo principal com a entrada pela linha de comando
+	public static void main (String entrada[])
+	{
+		//declaração de variaveis 
+		String nome = "Patrick Renan Marques Barboza";
+		String curso = "Análise e Desenvolvimento de Sistemas";
+		int idade = 21;
+		String genero = "Masculino";
+		double peso = 68;
+		//saida do resultado
+		System.out.println("Olá, meu nome é " + nome + ", estou cadastrado no curso de " + curso + " e tenho " + idade + " anos de idade, me identifico com o gênero " + genero + " e peso " + peso + "kg.");
+		System.exit(0);
+	}
+}
---
+++ MeuPrimeiroProgramaJava.java
@@ -0,0 +1,19 @@
+//salvar como MeuPrimeiroProgramaJava.java
+
+//nome da classe
+class MeuPrimeiroProgramaJava
+{
+	//módulo principal com a entrada pela linha de comando
+	public static void main (String entrada[])
+	{
+		//declaração de variaveis 
+		String nome = "Patrick Renan Marques Barboza";
+		String curso = "Análise e Desenvolvimento de Sistemas";
+		int idade = 21;
+		String genero = "Masculino";
+		double peso = 68;
+		//saida do resultado
+		System.out.println("Olá, meu Nome é " + nome + ", estou cadastrado no curso de " + curso + ", e tenho " + idade + "anos de idade e me identifico com o gênero " + genero + " e tenho o peso de " + peso + "kg.");
+		System.exit(0);
+	}
+}
